8ce3ffa2d3a56e791ecf1e20b09fccfc9b716d35
Skyler <skyler.m.degrote@outlook.com>
2016-08-28 15:40:06 -0500

    about page fixed - centered sections working and mobile friendly

--- about.php
+++ about.php
@@ -18,10 +18,14 @@ $pageTitle = "About";
 
     <br>
     <div class="section">
-        s1mething
+        <h3>About Just Write It</h3>
+        <p>Just Write It is a website filled with fiction writing resources. I hope to eventually cover more than just
+            fiction writing but for now that is what I know so that is what I'll include.</p>
     </div>
     <div class="section">
-        s2mething
+        <h3>About Author</h3>
+        <p>My Name is Skyler. I am a writer, programmer and cat lover. I've been writing since I was 12 and
+            I've been an author since I was 18.</p>
     </div>
 </div>
 
