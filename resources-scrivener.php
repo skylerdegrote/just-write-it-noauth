5a54e4e1401a914d9e75fcccc3ac62895a5ce61b
Skyler <skyler.m.degrote@outlook.com>
2016-09-06 19:13:10 -0500

    scrivener resources content added

--- resources-scrivener.php
+++ resources-scrivener.php
@@ -14,8 +14,8 @@ $pageTitle = "Scrivener as a Resource";
 </head>
 <body>
 <?php include ('includes/navigation.php') ?>
-<div class="container">
-    <div class="section scrivener">
+<div class="scrivenerContainer">
+    <div class="section">
         <div class="demo-container mdl-grid">
             <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
             <div class="mdl-color--white mdl-shadow--4dp">
