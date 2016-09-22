8d90da42bb52ae496fd2f7ece3f12c8b57174f59
Skyler <skyler.m.degrote@outlook.com>
2016-09-21 20:10:05 -0500

    creating cards dynamically started

--- resources_all.php
+++ resources_all.php
@@ -4,6 +4,10 @@
 $pageTitle = "All Resources";
 
 
+//array of titles
+$resourceTitle = ["The Write Practice", "Novel Now", "Twitter"];
+$resourceBody = ["The Write Practice is a great way to receive daily tips and tricks", "Novel Now is new to me but seems to have a lot of neat tricks!", "Twitter is amazing when it comes to inspiration for writers!"]
+
 ?>
 <!doctype html>
 <html>
@@ -15,7 +19,18 @@ $pageTitle = "All Resources";
 </head>
 <body>
 <?php include ('includes/navigation.php') ?>
-    <main class="container">
+    <main class="cardContainer">
+
+
+        <div class="card">
+            <div class="card-title">Title</div>
+            <div class="card-body">body text goes here</div>
+            <div class="card-footer">
+                <div class="card-link"><a href="index.php">Read More</a> </div>
+            </div>
+        </div>
+
+
         <div class="card">
             <div class="card-title">Title</div>
             <div class="card-body">body text goes here
@@ -25,6 +40,8 @@ $pageTitle = "All Resources";
                 <div class="card-link"><a href="index.php">Read More</a> </div>
             </div>
         </div>
+
+
         <div class="card">
             <div class="card-title">Title</div>
             <div class="card-body">body text goes here
@@ -34,6 +51,8 @@ $pageTitle = "All Resources";
                 <div class="card-link"><a href="index.php">Read More</a> </div>
             </div>
         </div>
+
+
         <div class="card">
             <div class="card-title">Title</div>
             <div class="card-body">body text goes here
@@ -43,15 +62,8 @@ $pageTitle = "All Resources";
                 <div class="card-link"><a href="index.php">Read More</a> </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-title">Title</div>
-            <div class="card-body">body text goes here
-                <div class="card-img"><img></div>
-            </div>
-            <div class="card-footer">
-                <div class="card-link"><a href="index.php">Read More</a> </div>
-            </div>
-        </div>
+
+
         <div class="card">
             <div class="card-title">Title</div>
             <div class="card-body">body text goes here
